bf88991346f6648cf1d8159f3c4cc230382a1532
Nick Devashvili <nikadevashvili@gmail.com>
2026-01-25 01:33:56 +0400

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import { ModalProvider } from "@/components/ModalContext";
 import GlobalModal from "@/components/GlobalModal";
 import { ThemeProvider } from "@/components/theme-provider";
-import { Analytics } from "@vercel/analytics/next";
 
 const jetbrainsMono = JetBrains_Mono({
     variable: "--font-jetbrains-mono",
